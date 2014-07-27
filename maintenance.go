62819e702e6ce13bd4f00c9dc73afcaea4214779
Jemy <jemygraw@gmail.com>
2014-07-27 22:10:42 +0800

    添加一个维护页面。

---
+++ maintenance.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(rw http.ResponseWriter, req *http.Request) {
+	var respData = "<html><body><h1 style='text-align:center;margin-top:20px;'>Go友团网站升级中。。。明天上午恢复正常。。。谢谢！ :)</h1></body></html>"
+	rw.Header().Add("Content-Type", "text/html;charset=utf-8")
+	rw.Write([]byte(respData))
+}
+
+func main() {
+	addr := "golanghome:80"
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Println(err)
+	}
+}
